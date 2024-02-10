e01e5ed0a348c0629800572ab8399ef786093094
Guga Zimmermann <gugazimmermann@gmail.com>
2024-02-10 12:49:52 -0300

    feat(auth): auth pages

--- src/pages/public/auth/Login.tsx
+++ src/pages/public/auth/Login.tsx
@@ -1,17 +1,23 @@
 import {useState} from "react";
+import {useParams, useNavigate} from "react-router-dom";
 import {type LoginValues} from "../../../common/interfaces/auth";
 import {type AlertProps} from "../../../common/interfaces/components";
 import {EN, ROUTES} from "../../../common/constants";
+import {Auth} from "../../../services";
+import {isUser} from "../../../common/helpers";
 import {EmailIcon, PasswordIcon} from "../../../icons";
 import {AuthLink} from "../../../components";
+import {validateEmail} from "../../utils/validation";
 import AuthLayout from "./AuthLayout";
 import AuthForm from "./AuthForm";
 
 const Login = (): JSX.Element => {
+  const navigate = useNavigate();
+  const {email} = useParams();
   const [loading, setLoading] = useState<boolean>(false);
   const [alert, setAlert] = useState<AlertProps>();
   const [values, setValues] = useState<LoginValues>({
-    email: "",
+    email: email ?? "",
     password: "",
   });
 
@@ -19,7 +25,20 @@ const Login = (): JSX.Element => {
     e.preventDefault();
     setLoading(true);
     setAlert(undefined);
-    console.log(values);
+    if (!validateEmail(values.email, setAlert)) {
+      setLoading(false);
+      return;
+    }
+    const response = await Auth.login(values);
+    if (!isUser(response?.data)) {
+      setAlert({
+        type: "error",
+        text: response.data === "Unverified" ? EN.AUTH.USERUNVERIFIED : EN.AUTH.USERUNAUTHORIZED,
+      });
+    } else {
+      localStorage.setItem("token", String(response.data.token));
+      navigate(ROUTES.DASHBOARD);
+    }
     setLoading(false);
   };
 
@@ -54,7 +73,7 @@ const Login = (): JSX.Element => {
         <>
           <AuthLink route={ROUTES.FORGOT_PASSWORD} text={EN.AUTH.LINKFORGOTPASSWORD} />
           <AuthLink route={ROUTES.REGISTER} text={EN.AUTH.LINKREGISTER} />
-          <AuthLink route={ROUTES.VERIFY_EMAIL} text={EN.AUTH.LINKFORGOTPASSWORD} />
+          <AuthLink route={ROUTES.VERIFY_EMAIL} text={EN.AUTH.LINKVERIFYEMAIL} />
         </>
       </AuthForm>
     </AuthLayout>
