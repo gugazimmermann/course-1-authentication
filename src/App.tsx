288b2f67549ece58ed6203cb3fcf8664d4c96398
Guga Zimmermann <gugazimmermann@gmail.com>
2024-02-10 10:10:31 -0300

    Revert "add Home, Blog, Login and Routes"

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./Home";
-import Blog from "./Blog";
-import Login from "./Login";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/entrar" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
